2e8687bc7a2c8927f518eec3cfc1ae3954001a5a
unknown <williangrillo.faculdade@gmail.com>
2021-03-11 17:03:58 -0300

    ContaBancaria

---
+++ ExercicioContaBancaria/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaBancaria1
+{
+    public class Cliente
+    {
+        public Cliente(string cpf, string nome)
+        {
+            Cpf = cpf;
+            Nome = nome;
+
+        }
+
+        public string Cpf { get; set; }
+        public string Nome { get; set; }
+        public Conta Conta{ get; set; }
+
+    }
+    
+
+
+
+}
+
